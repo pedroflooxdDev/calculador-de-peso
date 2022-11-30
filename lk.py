85988898c908846a8919a98a1b713363db159ddd
pedroflooxdDev <95549653+pedroflooxdDev@users.noreply.github.com>
2022-11-30 20:26:55 -0300

    lang
    g

--- lk.py
+++ lk.py
@@ -1,10 +1,23 @@
 from pyfiglet import Figlet
 import time
 
-f = Figlet(font='small')
-print (f.renderText('calculador de peso'))
-time.sleep(3)
+print("\t\t\tSelect a language\n\t\t\t-----------------------------\n\t\t\tSelecciona un lenguaje\n\n")
+print("\t\t\t[1] Español\n\t\t\t[[2] English")
 
-peso = input("ponga su peso aqui\n>> ")
+lang = input(">> ")
+if lang=="1":
+	f = Figlet(font='small')
+	print (f.renderText('calculador de peso'))
+	time.sleep(3)
 
-print('usted pesa %s kg' %peso)
+	peso = input("ponga su peso aqui\n>> ")
+
+	print('usted pesa %s kg' %peso)
+if lang=="2":
+	f = Figlet(font='small')
+	print (f.renderText('Weight Calculator'))
+	time.sleep(3)
+
+	peso = input("put your weight here\n>> ")
+
+	print('you weigh %s kg' %peso)
